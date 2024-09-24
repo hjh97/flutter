ad5427432d66145fdc43f4392a36183130f0b9a9
홍준혁 <hongjunhyeok@hongjunhyeog-ui-MacBookAir.local>
2024-09-24 20:03:00 +0900

    week3

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'EOS ToDoList',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
+        fontFamily: 'Pretendard',
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -27,54 +28,94 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightGreen,
+        backgroundColor: Colors.lightGreen.withOpacity(0.1),
         title: Text('EOS ToDoList'),
-        leading: Icon(Icons.check_box_outlined),
+        leading: Image.asset('assets/images/eos_logo.png'),
       ),
-      body: Container(
-        height: 200,
-        color: Colors.lightGreen.withOpacity(0.3),
-        padding: EdgeInsets.all(25),
-        child: Row(
-          children: [
-            Container(
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    width: 10,
-                    color: Colors.grey,
-                  )),
-              child: Center(
-                child: Icon(
-                  Icons.person,
-                  size: 100,
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('EOS'),
-                Text(
-                  '홍준혁',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text('홍준혁입니다.'),
-              ],
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          _buildCard(),
+          _toDoList(context),
+        ],
       ),
     );
   }
+}
+
+Widget _buildCard() {
+  return Container(
+    height: 200,
+    color: Colors.lightGreen.withOpacity(0.2),
+    padding: EdgeInsets.all(25),
+    child: Row(
+      children: [
+        Container(
+          height: 150,
+          width: 150,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              width: 10,
+              color: Colors.grey,
+            ),
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: Center(
+            child: Image.asset('assets/images/eos_logo.png'),
+          ),
+        ),
+        SizedBox(
+          width: 35,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '홍준혁',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text('감사합니다'),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _toDoList(BuildContext context) {
+  var height = MediaQuery.of(context).size.height;
+  var width = MediaQuery.of(context).size.width;
+  return Stack(
+    children: [
+      Container(
+          margin: EdgeInsets.all(20),
+          height: height - 355,
+          width: width,
+          decoration: BoxDecoration(
+            color: Colors.lightGreen.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(20),
+          )),
+      Center(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 3),
+          margin: EdgeInsets.all(40),
+          decoration: BoxDecoration(
+              color: Colors.lightGreen.withOpacity(0.3),
+              borderRadius: BorderRadius.circular(20)),
+          child: Text(
+            "To Do List",
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 15, color: Colors.white),
+          ),
+        ),
+      ),
+    ],
+  );
 }
